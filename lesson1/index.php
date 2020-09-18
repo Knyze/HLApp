2106db711458dc21d3a53b494eea4371fd9aa28b
Knyze <kn-dmitriy@yandex.ru>
2020-09-18 11:59:57 +0500

    lesson1

---
+++ lesson1/index.php
@@ -0,0 +1,14 @@
+<?php
+$text = 'Здесь должен быть какой-нибудь скрипт';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 1</title>
+</head>
+<body>
+    <h1><?=$text ?></h1>
+</body>
+</html>
