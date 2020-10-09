1dea1b86a1b1206e8de13937151e3c3228424cef
Knyze <kn-dmitriy@yandex.ru>
2020-10-09 17:07:00 +0500

    lesson6 Ссылки бессмысленно отправлять

--- lesson6/HighLoadApp/index.php
+++ lesson6/HighLoadApp/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Это сервер 1';
+$m = new Memcached ();
+$m->addServer('localhost', 11211);
+$server = $m->get('server');
+$m->set('server', 'server 1');
+
+echo 'Это сервер 1' . PHP_EOL;
+echo 'До это вы были на ' . $server;
--- lesson6/HighLoadApp2/index.php
+++ lesson6/HighLoadApp2/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Это сервер 2';
+$m = new Memcached();
+$m->addServer('192.168.0.14', 11211);
+
+echo 'Это сервер 2' . PHP_EOL;
+
+if ($server = $m->get('server')) {
+    echo 'До это вы были на ' . $server;
+}
+
+$m->set('server', 'server 2');
